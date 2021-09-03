947c0d3150304015f43377552e9b0d48fc5a18e6
Vitaliy Zarubin <keygenqt@gmail.com>
2021-09-03 10:11:59 +0300

    Added AppArgParser, update readme

--- src/main/kotlin/com/keygenqt/autoway/common/config/Application.kt
+++ src/main/kotlin/com/keygenqt/autoway/common/config/Application.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+ 
 package com.keygenqt.autoway.common.config
 
 import com.keygenqt.autoway.api.routing.apiRoute
